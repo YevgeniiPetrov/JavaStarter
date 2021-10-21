ec71faf2821c27a9752729b6d1c0c3f83ef4895a
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2021-10-21 23:45:48 +0300

    homework 2 is done

--- src/com/itvdn/javastarter/petrov/lesson002/Task002.java
+++ src/com/itvdn/javastarter/petrov/lesson002/Task002.java
@@ -1,7 +1,17 @@
+/* Задание 2
+Используя IntelliJ IDEA, создайте проект.
+Дано значение числа pi, которое равно 3,141592653 и значение числа Эйлера е, которое равно
+2,7182818284590452. Создайте две переменные, присвойте им значения числа pi и числа е и выведите
+их на экран без потери точности.*
+* */
+
 package com.itvdn.javastarter.petrov.lesson002;
 
 public class Task002 {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        double pi = 3.141592653;
+        String e = "2.7182818284590452";
+        System.out.println(pi);
+        System.out.println(e);
     }
 }
---
+++ src/com/itvdn/javastarter/petrov/lesson002/Task003.java
@@ -0,0 +1,22 @@
+/*Задание 3
+Используя IntelliJ IDEA, создайте проект.
+Создайте три строковые переменные и присвойте им значения:
+"\nмоя строка 1"
+"\tмоя строка 2"
+"\aмоя строка 3"
+Выведите значение каждой переменной на экран. Какие отличия вы увидели. Сделайте выводы.
+* */
+
+package com.itvdn.javastarter.petrov.lesson002;
+
+public class Task003 {
+    public static void main(String[] args) {
+        String s1 = "\nмоя строка 1";
+        String s2 = "\tмоя строка 2";
+        String s3 = "моя строка 3";
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
