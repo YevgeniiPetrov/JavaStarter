13613a7246910d0fdd3625101c2378d4a88b1d1b
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2021-10-29 01:12:36 +0300

    homework 5

---
+++ src/com/itvdn/javastarter/petrov/lesson005/homework/PowerOfTwo.java
@@ -0,0 +1,19 @@
+/* Задание 1
+Известно, что у чисел, которые являются степенью двойки, только один бит имеет значение 1.
+Используя IntelliJ IDEA, создайте класс PowerOfTwo.
+Напишите программу, которая будет выполнять проверку – является ли указанное число степенью
+двойки или нет.*/
+
+package com.itvdn.javastarter.petrov.lesson005.homework;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = sc.nextInt();
+        sc.close();
+        System.out.println("Число " + number + ((number & (number - 1)) == 0 ? " " : " не ") + "является степенью двойки.");
+    }
+}
