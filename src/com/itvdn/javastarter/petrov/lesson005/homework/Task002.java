0f374daa588a88889f20ac91c7fd293140965ba6
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2021-10-28 23:34:52 +0300

    init task 2

---
+++ src/com/itvdn/javastarter/petrov/lesson005/homework/Task002.java
@@ -0,0 +1,13 @@
+/* Задание **
+Написать таблицу истинности кодом для знаков
+^, |, &.
+Что такое таблица истинности, если вы не знаете - загуглить самостоятельно.
+*/
+
+package com.itvdn.javastarter.petrov.lesson005.homework;
+
+public class Task002 {
+    public static void main(String[] args) {
+        
+    }
+}
