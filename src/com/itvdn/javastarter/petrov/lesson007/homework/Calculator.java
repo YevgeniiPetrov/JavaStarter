46cf28322dddd8f97e8320c44e7d55c90d79dc0c
Yevgenii Petrov <petrov.yevgenii@gmail.com>
2021-11-06 02:01:34 +0200

    tasks Arithmetics and Calculator are changed

--- src/com/itvdn/javastarter/petrov/lesson007/homework/Calculator.java
+++ src/com/itvdn/javastarter/petrov/lesson007/homework/Calculator.java
@@ -6,12 +6,12 @@ package com.itvdn.javastarter.petrov.lesson007.homework;
 
 public class Calculator {
     public static double calculate(int firstNumber, int secondNumber, int thirdNumber) {
-        return (firstNumber + secondNumber + thirdNumber) / 3;
+        return (firstNumber + secondNumber + thirdNumber) / 3.0;
     }
 
     public static void main(String[] args) {
         System.out.println(calculate(1, 2, 3));
-        System.out.println(calculate(4, 5, 6));
+        System.out.println(calculate(4, 4, 3));
         System.out.println(calculate(7, 8, 9));
     }
 }
