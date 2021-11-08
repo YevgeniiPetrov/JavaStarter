b84965018daa7312a6ca5e57f1396767dd842b4c
Петров Євген Сергійович <petrov.yes@np.work>
2021-11-08 20:19:23 +0200

    homework 8 (part 2) is done

--- src/com/itvdn/javastarter/petrov/lesson008/homework/Bank.java
+++ src/com/itvdn/javastarter/petrov/lesson008/homework/Bank.java
@@ -11,7 +11,19 @@
 package com.itvdn.javastarter.petrov.lesson008.homework;
 
 public class Bank {
-    public static void main(String[] args) {
+    public static void creditInfo(int creditAmount, int creditPeriod, int amountOfPayments, int amountOfCurrentPayment, int paymentNumber) {
+        double monthlyPayment = (double) creditAmount / creditPeriod;
+        double amountOwed = (paymentNumber - 1) * monthlyPayment - amountOfPayments + amountOfCurrentPayment;
+        boolean isAmountOwed = amountOwed > 0;
+        System.out.printf("Состояние кредта:\n- сумма кредита: %d;\n- кредит взят на период: %d;\n- сделано платежей: %d;\n- минимальная сумма платежа: %.2f;\n", creditAmount, creditPeriod, paymentNumber, monthlyPayment);
+        System.out.printf("- сумма задолженности: %.2f;\n- сумма переплаты: %.2f;\n- отсутствие долга: %s.\n", isAmountOwed ? amountOwed : 0, Math.abs(isAmountOwed ? 0 : amountOwed), (isAmountOwed ? "нет" : "дат"));
+    }
 
+    public static void creditInfo(int amountOfCurrentPayment) {
+        creditInfo(3000, 15, 400, amountOfCurrentPayment, 5);
+    }
+
+    public static void main(String[] args) {
+        creditInfo(300);
     }
 }
